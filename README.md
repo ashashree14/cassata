d83b532a4e7903da5b7d06aa5b1156c14bd14942
Sunil Kumar C <sunil10790@gmail.com>
2016-01-07 07:24:13 +0530

    US12: Add controller to display movie details

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ This is a front end application built on AngularJS displaying the movie names al
 
 ## Plugins used:
 1. EditorConfig
+
+## Frameworks used:
+1. AngularJS
+2. Bootstrap
--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var app = angular.module('cassata', []);
+(function(){
+    var app = angular.module('cassata', []);
+
+    app.controller('MovieController', function(){
+        this.movie = movie;
+    });
+
+    var movie = {
+        name: 'Inception',
+        year: 1990,
+        contentRating: 'PG-13',
+        genre: ['Action', 'Mystery', 'Sci-Fi'],
+        onlyRating: 8.8,
+        isSequence: false
+    };
+})();
